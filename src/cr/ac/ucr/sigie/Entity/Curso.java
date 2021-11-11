e9a3c39e8e893f812d59d0ee257d5a15d0c9c52e
FlameJordan <carangatasonic@gmail.com>
2021-11-11 10:55:41 -0600

    Se agregaron los atributos electivos, requisitos, correquisitos, area, resultadoAprendizaje, itemesDescripcion, referencias, modalidad, contenidos, unidadAcademicaPropietarias

--- src/cr/ac/ucr/sigie/Entity/Curso.java
+++ src/cr/ac/ucr/sigie/Entity/Curso.java
@@ -1,5 +1,7 @@
 package cr.ac.ucr.sigie.Entity;
 
+import java.util.ArrayList;
+
 public class Curso {
 
     private int cursoId;
@@ -13,6 +15,17 @@ public class Curso {
     private int horasPractica;
     private int horasLaboratorio;
     private int horasTeoriaPractica;
+    private ArrayList<Curso> electivos;
+    private ArrayList<Curso> requisitos;
+    private ArrayList<Curso> correquisitos;
+    private Area area;
+    private ArrayList<ResultadoAprendizaje> resultadoAprendizaje;
+    private ArrayList<ItemDescripcion> itemesDescripcion;
+    private ArrayList<ReferenciaBibliografica> referencias;
+    private Modalidad modalidad;
+    private ArrayList<Contenido> contenidos;
+    private ArrayList<UnidadAcademica> unidadAcademicaPropetarias;
+
 
     public Curso() {
         this.cursoId = 0;
@@ -26,11 +39,20 @@ public class Curso {
         this.horasPractica = 0;
         this.horasLaboratorio = 0;
         this.horasTeoriaPractica = 0;
+        this.electivos = null;
+        this.requisitos = null;
+        this.correquisitos = null;
+        this.area = null;
+        this.resultadoAprendizaje = null;
+        this.itemesDescripcion = null;
+        this.referencias = null;
+        this.modalidad = null;
+        this.contenidos = null;
+        this.unidadAcademicaPropetarias = null;
     }
 
 
-
-    public Curso(int cursoId, String nombre, int ciclo, String sigla, int creditos, String objetivoGeneral, int anoEnQueSeImparte, int horasTeoria, int horasPractica, int horasLaboratorio, int horasTeoriaPractica) {
+    public Curso(int cursoId, String nombre, int ciclo, String sigla, int creditos, String objetivoGeneral, int anoEnQueSeImparte, int horasTeoria, int horasPractica, int horasLaboratorio, int horasTeoriaPractica, ArrayList<Curso> electivos, ArrayList<Curso> requisitos, ArrayList<Curso> correquisitos, Area area, ArrayList<ResultadoAprendizaje> resultadoAprendizaje, ArrayList<ItemDescripcion> itemesDescripcion, ArrayList<ReferenciaBibliografica> referencias, Modalidad modalidad, ArrayList<Contenido> contenidos, ArrayList<UnidadAcademica> unidadAcademicaPropetarias) {
         this.cursoId = cursoId;
         this.nombre = nombre;
         this.ciclo = ciclo;
@@ -42,6 +64,16 @@ public class Curso {
         this.horasPractica = horasPractica;
         this.horasLaboratorio = horasLaboratorio;
         this.horasTeoriaPractica = horasTeoriaPractica;
+        this.electivos = electivos;
+        this.requisitos = requisitos;
+        this.correquisitos = correquisitos;
+        this.area = area;
+        this.resultadoAprendizaje = resultadoAprendizaje;
+        this.itemesDescripcion = itemesDescripcion;
+        this.referencias = referencias;
+        this.modalidad = modalidad;
+        this.contenidos = contenidos;
+        this.unidadAcademicaPropetarias = unidadAcademicaPropetarias;
     }
 
     public int getCursoId() {
@@ -130,5 +162,85 @@ public class Curso {
 
     public void setHorasTeoriaPractica(int horasTeoriaPractica) {
         this.horasTeoriaPractica = horasTeoriaPractica;
+    }
+
+    public ArrayList<Curso> getElectivos() {
+        return electivos;
+    }
+
+    public void setElectivos(ArrayList<Curso> electivos) {
+        this.electivos = electivos;
+    }
+
+    public ArrayList<Curso> getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(ArrayList<Curso> requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public ArrayList<Curso> getCorrequisitos() {
+        return correquisitos;
+    }
+
+    public void setCorrequisitos(ArrayList<Curso> correquisitos) {
+        this.correquisitos = correquisitos;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public ArrayList<ResultadoAprendizaje> getResultadoAprendizaje() {
+        return resultadoAprendizaje;
+    }
+
+    public void setResultadoAprendizaje(ArrayList<ResultadoAprendizaje> resultadoAprendizaje) {
+        this.resultadoAprendizaje = resultadoAprendizaje;
+    }
+
+    public ArrayList<ItemDescripcion> getItemesDescripcion() {
+        return itemesDescripcion;
+    }
+
+    public void setItemesDescripcion(ArrayList<ItemDescripcion> itemesDescripcion) {
+        this.itemesDescripcion = itemesDescripcion;
+    }
+
+    public ArrayList<ReferenciaBibliografica> getReferencias() {
+        return referencias;
+    }
+
+    public void setReferencias(ArrayList<ReferenciaBibliografica> referencias) {
+        this.referencias = referencias;
+    }
+
+    public Modalidad getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(Modalidad modalidad) {
+        this.modalidad = modalidad;
+    }
+
+    public ArrayList<Contenido> getContenidos() {
+        return contenidos;
+    }
+
+    public void setContenidos(ArrayList<Contenido> contenidos) {
+        this.contenidos = contenidos;
+    }
+
+    public ArrayList<UnidadAcademica> getUnidadAcademicaPropetarias() {
+        return unidadAcademicaPropetarias;
+    }
+
+    public void setUnidadAcademicaPropetarias(ArrayList<UnidadAcademica> unidadAcademicaPropetarias) {
+        this.unidadAcademicaPropetarias = unidadAcademicaPropetarias;
     }
 }
